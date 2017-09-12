ce186e13312c513626302dc3bfb63dd94d6ef2d2
jerry_zj <jerrylzj810410738@gmail.com>
2017-09-12 19:55:24 +0800

    初始创建项目

--- Card/settings.py
+++ Card/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'dj_zq9c&gj#=d41+pxchx#tv@3q$uv=)peewp_1-+ibl%l%szz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'CardSystem',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
